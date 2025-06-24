57f33c48e4f933be6cf9d9cccc3581835fc6680d
Jake Emitte <jakeemitte829@gmail.com>
2025-06-23 19:16:24 -0700

    Initial Commit

---
+++ SelfTest/SelfTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfTestApp.swift
+//  SelfTest
+//
+//  Created by Jake Emitte on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SelfTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
