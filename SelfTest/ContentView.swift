ff9db3adcdc224e54401d4c11454f32a0a72cee8
Jake Emitte <jakeemitte829@gmail.com>
2025-06-23 19:36:51 -0700

    I wanted to challenge myself to the one button project from scrath, no refrences. took about 10 minutes. personal challenge passed.

--- SelfTest/ContentView.swift
+++ SelfTest/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(radius: 20)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Spacer()
+            Button("Press Me!") {
+                let message1 = "ON"
+                let message2 = "OFF"
+                message = message == message1 ? message2 : message1
+                imageName = ("image\(imageNumber)")
+                if imageNumber >= 9 {
+                    imageNumber = 0
+                }
+                imageNumber = imageNumber + 1
+                
+                
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
         .padding()
     }
